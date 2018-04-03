bc1679af53ee20037d1ca90ef0f12f878d121131
showmen15 <showmen15@poczta.onet.pl>
2018-04-03 13:08:59 +0200

    automatyzacja Regex-ow

--- PdfChecker/Form1.cs
+++ PdfChecker/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -42,6 +44,76 @@ namespace PdfChecker
                 }
             }
         }
-    }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sTextFileDirectory = @"J:\Doktorat\DocPublish";
+
+            RegexChecker reg = new RegexChecker();
+            StringBuilder Log = new StringBuilder();
+
+            List<string> getAllTexFile = new List<string>(Directory.GetFiles(sTextFileDirectory, "*.tex", SearchOption.AllDirectories));
+            List<string> getOldTexFile = new List<string>(Directory.GetFiles(sTextFileDirectory, "*_old.tex", SearchOption.AllDirectories));
+
+            //wykluczenia ze sprawdzania 
+            getOldTexFile.Add(string.Format("{0}\\{1}", sTextFileDirectory, @"lookup\lookup.tex")); //ze wzgledu na linki
+            getOldTexFile.Add(string.Format("{0}\\{1}", sTextFileDirectory, @"06_experimental_results\simulation\experimental-result-simulation-table.tex")); //ze wzgledu na tabele
+            getOldTexFile.Add(string.Format("{0}\\{1}", sTextFileDirectory, @"06_experimental_results\robot\experimental-result-robot-table.tex")); //ze wzgledu na tabele
+
+            getOldTexFile.Add(string.Format("{0}\\{1}", sTextFileDirectory, @"05_hybrid_algorithm\FearFactorBase.tex")); //ze wzgledu na wzory
+            getOldTexFile.Add(string.Format("{0}\\{1}", sTextFileDirectory, @"05_hybrid_algorithm\FactorPassageBy.tex")); //ze wzgledu na wzory
+
+
+            foreach (var item in getOldTexFile)      //usun old oraz  wykluczone
+                getAllTexFile.Remove(item);
+
+            // string[] sFileList = new string[] { @"J:\Doktorat\DocPublish\01_introduction\introduction_old.tex" }; //getAllTexFile.ToArray(); 
+            string[] sFileList = getAllTexFile.ToArray(); 
+
+            foreach (var item in sFileList)            
+                Log.AppendLine(reg.CheckFile(item));
+
+            txtRegexLog.AppendText(Log.ToString());
+
+        }
+
+            ///*string sTestedLog;
+            //string[] sTestedTexLines;
+
+            //string sPatternCytowania = @"(\s\-\s)";
+            ////Regex reg = Regex.
+
+            //foreach (var file in sFileList)
+            //{
+            //    //sTestedLog = File.ReadAllText(file);
+
+            //    sTestedTexLines = File.ReadAllLines(file);
+
+            //    for (int i = 0; i < sTestedTexLines.Length; i++)
+            //    {
+            //        MatchCollection matchCollection = Regex.Matches(sTestedTexLines[i], sPatternCytowania);
+
+            //        foreach (var match in matchCollection)
+            //        {
+
+            //        }
+            //    }
+
+               
+
+            //    //MatchCollection matchCollection =  Regex.Matches(sTestedLog, sPatternCytowania);
+
+            //    //foreach (var match in matchCollection)
+            //    /*{
+
+            //    }*/
+
+            //}
+
+
+       
+
+       
+
+    }
 }
---
+++ PdfChecker/RegexChecker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PdfChecker
+{
+    public class RegexChecker
+    {
+        
+        Regex regCytowania = new Regex(@"\\cite\{[A-Za-z0-9,\s]+\}\s+\.");
+        Regex regMyslniki = new Regex(@"(\s\-\s)");
+
+
+        Regex regAlgorytmyLinkReplace = new Regex(@"((((\\lookupGetHref)|(\\lookupGetUrl)|(\\lookupGetHrefURL)|(\\lookupPut))\{[\w+\s-\n\.]+\}))|(\$\sR\s\$)");
+        Regex regAlgorytmy = new Regex(@"(\s)((R)|(R\+)|(PF)|(PF\+)|(RVO)|(PR))((\s)|(\.))");
+
+        public RegexChecker()
+        {
+
+        }
+
+        public string CheckFile(string sFilePath)
+        {
+            StringBuilder log = new StringBuilder();
+            MatchCollection matchCollection;
+            string input;
+
+            string sFileName = sFilePath; //Path.GetFileNameWithoutExtension(sFilePath);
+            string[] sTextLines = File.ReadAllLines(sFilePath);
+
+            for (int i = 0; i < sTextLines.Length; i++)
+            {
+                matchCollection = regCytowania.Matches(sTextLines[i]);
+                if (matchCollection.Count > 0)
+                    log.AppendLine(logPattern(sFileName, matchCollection, "regCytowania", i));
+
+                matchCollection = regMyslniki.Matches(sTextLines[i]);
+                if (matchCollection.Count > 0)
+                    log.AppendLine(logPattern(sFileName, matchCollection, "regMyslniki", i));
+
+                input = sTextLines[i];
+                input = regAlgorytmyLinkReplace.Replace(input, "REPLACE:LINK");
+                matchCollection = regAlgorytmy.Matches(input);
+                if (matchCollection.Count > 0)
+                    log.AppendLine(logPattern(sFileName, matchCollection, "regAlgorytmy", i));
+            }
+
+            return log.ToString();
+        }
+
+        private string logPattern(string sFileName, MatchCollection matchCollection, string sPattern, int lineNr)
+        {
+            return string.Format("({0}: {1}) ==> {2}", sFileName, lineNr, sPattern);
+        }
+    }
+}
