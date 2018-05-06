cce03389f80217d03152de1e9ca14e3a34dde725
szsz <showmen15@poczta.onet.pl>
2018-05-06 18:58:33 +0200

    test czy pliki istenieją

--- PdfChecker/Form1.Designer.cs
+++ PdfChecker/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.numEnd = new System.Windows.Forms.NumericUpDown();
             this.button3 = new System.Windows.Forms.Button();
             this.txtRegexLog = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numBegin)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numEnd)).BeginInit();
             this.SuspendLayout();
@@ -121,11 +122,22 @@
             this.txtRegexLog.Size = new System.Drawing.Size(789, 315);
             this.txtRegexLog.TabIndex = 6;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(702, 279);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(813, 635);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.txtRegexLog);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.numEnd);
@@ -151,6 +163,7 @@
         private System.Windows.Forms.NumericUpDown numEnd;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox txtRegexLog;
+        private System.Windows.Forms.Button button4;
     }
 }
 
