715feb08bca92b2585b53deb08901a503d38120d
showmen15 <showmen15@poczta.onet.pl>
2018-05-24 08:43:12 +0200

    wylaczenie nazwy przypadkow

--- ExcelTest2/ExcelTest/BoxPlotPDF2.R
+++ ExcelTest2/ExcelTest/BoxPlotPDF2.R
@@ -5,7 +5,8 @@ dat <- read.csv(file=filenameInput, header=FALSE, sep=",")
 pdf(filenameOutput, encoding="ISOLatin2")
 
 #boxplot(dat, col=c("blue", "green", "red", "white","orange","yellow"), names=c("R","PF","RVO","PR","R+","PF+"),  main=chartName, xlab="Algorithm name", ylab="Total time for all robots" )
-boxplot(dat, col=c("blue", "green", "red", "white","orange","yellow"), names=c("R","PF","RVO","PR","R+","PF+"),  main=chartName, xlab="Nazwa algorytmu", ylab="£¹ czny czas dla wszystkich robotów"  )
+#boxplot(dat, col=c("blue", "green", "red", "white","orange","yellow"), names=c("R","PF","RVO","PR","R+","PF+"),  main=chartName, xlab="Nazwa algorytmu", ylab="£¹ czny czas dla wszystkich robotów"  )
+boxplot(dat, col=c("blue", "green", "red", "white","orange","yellow"), names=c("R","PF","RVO","PR","R+","PF+"),  xlab="Nazwa algorytmu", ylab="£¹ czny czas dla wszystkich robotów"  )
 #mtext(text=testResult, side=4, adj = 0, cex=0.7)
 
 }
